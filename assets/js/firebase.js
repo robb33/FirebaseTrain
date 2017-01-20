1bf84e27615a5d50da8e26aa941b60241c23a9ad
rob bee <rbenny@earthlink.net>
2017-01-20 18:18:27 -0500

    testing FB/Moment

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,3 +1,17 @@
+
+(function () {
+  
+// use Moment.js to display current date in a div (#displayMoment)
+
+var now = moment();
+
+
+var eDisplayMoment = document.getElementById('displayMoment');
+eDisplayMoment.innerHTML = now.format('D MMM YYYY h:mm a');
+})();
+
+
+
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCrSSIa75Z_tRPdFlJf41l_MqELRlLcNj0",
@@ -16,14 +30,23 @@
 
  // variables for schedule 
 
-
  var bigOne = document.getElementById('bigOne'); //header test
  var trainName = document.getElementById('trainName'); // trainName
  var dest = document.getElementById('dest'); // destination
- var firstTrainTime = document.getElementById('firstTrainTime'); // firstTrainTime
+ var first = document.getElementById('first'); // firstTrainTime
  var freq = document.getElementById('freq'); // frequency
+ var minAway = document.getElementById('minAway'); // frequency
 
+//console log variables
 
+console.log(bigOne);
+console.log(trainName);
+console.log(dest);
+console.log(first);
+console.log(freq);
+console.log(minAway);
+
+//html output
 
 var dbRef = firebase.database().ref().child('trainName');
  dbRef.on('value', snap => trainName.innerText = snap.val());
@@ -31,14 +54,25 @@ var dbRef = firebase.database().ref().child('trainName');
 var dbRef = firebase.database().ref().child('dest');
  dbRef.on('value', snap => dest.innerText = snap.val());
  
-var dbRef = firebase.database().ref().child('firstTrainTime');
- dbRef.on('value', snap => firstTrainTime.innerText = snap.val());
+var dbRef = firebase.database().ref().child('first');
+ dbRef.on('value', snap => first.innerText = snap.val());
 
 var dbRef = firebase.database().ref().child('freq');
  dbRef.on('value', snap => freq.innerText = snap.val());
 
 var dbRef = firebase.database().ref().child('text');
  dbRef.on('value', snap => bigOne.innerText = snap.val());
+
+ var dbRef = firebase.database().ref().child('minAway');
+ dbRef.on('value', snap => minAway.innerText = snap.val());
+
+
+
+
+
+
+
+
 
 
 
